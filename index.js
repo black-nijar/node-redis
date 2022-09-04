c0df14fe57b4146f3fb74db24e639d60eeb357e5
black-nijar <nijarr2020@gmail.com>
2022-09-04 20:16:02 +0530

    basic redis done

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-
+const redis = require("redis");
 const app = express();
+
+// create redis client
+const redisClient = redis.createClient();
+redisClient.connect();
+const DEFAULT_EXP = 360;
+
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/photos", async (req, res) => {
-  const albumId = req.query.albumId;
-  const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/posts",
-    { params: { albumId } }
-  );
-  res.json(data);
+  try {
+    const redisData = await redisClient.get("photos");
+    if (redisData) {
+      res.json(JSON.parse(redisData));
+    } else {
+      const albumId = req.query.albumId;
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts",
+        { params: { albumId } }
+      );
+      redisClient.setEx("photos", DEFAULT_EXP, JSON.stringify(data));
+      res.json(data);
+    }
+  } catch (error) {
+    throw error;
+  }
 });
 
 app.get("/photos/:id", async (req, res) => {
